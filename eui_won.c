706bcd9419f47ecb6f2f79df74265d7bfc09c727
jooeui <38203369+jooeui@users.noreply.github.com>
2019-11-26 21:00:00 +0900

    final file
    final~~~~~~~~~~~~

--- eui_won.c
+++ eui_won.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
 
 int ew_getargs(char *cmd, char **argv);
 void ew_exit();
+void ew_ctrlc(int sig);
+void ew_ctrlz(int sig, int flag);
 void ew_cd(int argc, char* argv[]);
 void ew_mkdir(int argc, char *argv[]);
 void ew_rmdir(int argc, char *argv[]);
@@ -22,12 +25,19 @@ void main(){
     char buf[256];
     char *argv[50];
     int narg;
+    struct sigaction ctrlc_act;
+    struct sigaction ctrlz_act;
+    ctrlc_act.sa_handler = ew_ctrlc;
+    ctrlz_act.sa_handler = ew_ctrlz;
+    sigaction(SIGINT, &ctrlc_act, NULL);
+    sigaction(SIGTSTP, &ctrlz_act, NULL);
     pid_t pid;
 
     while(1){
-        printf("shell> ");
+        printf("[eui_won shell] ");
+        pwd_print();
         gets(buf);
-        narg = ew_getargs(buf, argv);
+        narg = ew_getargs(buf, argv);   // 들어온 인자 개수
         pid = fork();
 
         if(pid == 0)
@@ -39,6 +49,8 @@ void main(){
 
         if(!strcmp(buf, "exit"))
             ew_exit();
+        else if(!strcmp(buf, "pwd"))
+            ew_pwd(narg, argv);
         else if(!strcmp(buf, "cd"))
             ew_cd(narg, argv);
         else if(!strcmp(buf, "mkdir"))
@@ -51,8 +63,21 @@ void main(){
             ew_rm(narg, argv);
         else if(!strcmp(buf, "mv"))
             ew_mv(narg, argv);
-        else if(!strcmp(buf, "cat"))
-            ew_cat(narg, argv);
+
+        for (int i = 0; i < narg; i++) {
+            // -1 = '&', 1 = '|', 2 = '<', 3 = '>'
+            int t_opt = checkTaskOption(argv[i + 1]);
+
+            if(t_opt == 1){
+                run_pipe(i, argv);
+                i += 2;
+            }
+            else
+                run(i, t_opt, narg, argv);
+
+            if(t_opt > 1)
+                i += 2;
+        }
     }
 }
 
@@ -73,8 +98,51 @@ int ew_getargs(char *cmd, char **argv){
     return narg;
 }
 
+//실행 인자 포함 여부 확인. 0 = 없음, -1 = '&', 1 = '|', 2 = '<', 3 = '>'
+int checkTaskOption(char *argv){
+    int opt = 0;
+    if(argv == NULL)
+        return opt;
+
+    for(int i=0; argv[i] != NULL; i++){
+        if (argv[i] == '&'){
+            opt = -1;
+            return opt;
+        }
+        if (argv[i] == '|'){
+            opt = 1;
+            return opt;
+        }
+        if (argv[i] == '<'){
+            opt = 2;
+            return opt;
+        }
+        if (argv[i] == '>'){
+            opt = 3;
+            return opt;
+        }
+    }
+
+    return opt;
+}
+
 void ew_exit(){
     exit(1);
+}
+
+void ew_ctrlc(int sig){
+    signal(sig, SIG_IGN);
+    printf("  ctrl_C 입력 - 쉘 종료\n");
+    exit(1);
+}
+
+void ew_ctrlz(int sig, int flag){
+    signal(sig, SIG_IGN);
+    printf(" 쉘 일시정지..\n");
+    printf(" fg 명령으로 재개 가능..\n");
+    raise(SIGSTOP);
+    printf(" 쉘 재개..\n");
+    signal(sig, ew_ctrlz);
 }
 
 void ew_cd(int argc, char *argv[]){
@@ -89,25 +157,16 @@ void ew_cd(int argc, char *argv[]){
 }
 
 void ew_mkdir(int argc, char *argv[]){
-    if(argc < 2){
-      fprintf(stderr, "인자가 부족합니다.\n");
-      return;
-   }
    if(!(mkdir(argv[1], 0755)))
       fprintf(stderr, "디렉터리를 만들지 못했습니다.\n");
 }
 
 void ew_rmdir(int argc, char *argv[]){
-    if(argc < 2){
-      fprintf(stderr, "인자가 부족합니다.\n");
-      return;
-   }
-
    rmdir(argv);
 }
 
 void ew_cp(int argc, char *argv[]){
-    FILE *fp1;
+   FILE *fp1;
    FILE *fp2;
    char check;
 
@@ -140,7 +199,7 @@ void ew_rm(int argc, char *argv[]){
 }
 
 void ew_mv(int argc, char *argv[]){
-    FILE *fp1;
+   FILE *fp1;
    FILE *fp2;
    char check;
 
@@ -169,16 +228,216 @@ void ew_cat(int argc, char *argv[]){
    int check;
    FILE *fp;
 
-   if(argc < 2){
-      fprintf(stderr, "인자가 부족합니다.");
-      return;
-   }
-
    fp = fopen(argv[1], "r");
 
    while((check = getc(fp)) != EOF){
       putchar(check);
    }
-
    fclose(fp);
+
+}
+
+void ew_redirect(char *input_tem[]){
+    int fd[2];
+    int fd_o;
+    char *p_p[2];
+}
+
+void run(int i, int t_opt, int argc, char **argv){
+    pid_t pid;
+    int fd; /* file descriptor */
+    char *buf[1024];
+    int flags = O_RDWR | O_CREAT;
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; /* == 0644 */
+    memset(buf, 0, 1024);
+    pid = fork();
+    if (pid == 0){  //child
+        //-1 = &, 1 = pipe, 2 = <, 3 = >
+        if(t_opt == -1){
+            printf("%s가 백그라운드로 실행...\n", argv[i]);
+            selectCmd(i, argv);
+            exit(0);
+        }
+        else if(t_opt == 2){
+            if ((fd = open(argv[i + 2], flags, mode)) == -1) {
+                perror("open"); /* errno에 대응하는 메시지 출력됨*/
+                exit(1);
+            }
+            if (dup2(fd, STDIN_FILENO) == -1) {
+                perror("dup2"); /* errno에 대응하는 메시지 출력됨 */
+                exit(1);
+            }
+            if (close(fd) == -1) {
+                perror("close"); /* errno에 대응하는 메시지 출력됨*/
+                exit(1);
+            }
+            ew_cat(argc, argv[i+2]);
+            selectCmd(i, argv);
+            exit(0);
+        }
+        else if(t_opt == 3){
+            if ((fd = open(argv[i+2], flags, mode)) == -1) {
+                perror("open"); /* errno에 대응하는 메시지 출력됨*/
+                exit(1);
+            }
+            if (dup2(fd, STDOUT_FILENO) == -1) {
+                perror("dup2"); /* errno에 대응하는 메시지 출력됨 */
+                exit(1);
+            }
+            if (close(fd) == -1) {
+                perror("close"); /* errno에 대응하는 메시지 출력됨*/
+                exit(1);
+            }
+            selectCmd(i, argv);
+            exit(0);
+        }
+    }
+    else if (pid > 0){  //parent - 백그라운드 아닐 때만 기다림
+        if(t_opt >= 0){ //백그라운드가 아닐 때
+            wait(pid);
+       }
+        if(!strcmp(argv[i], "cd")){
+            ew_cd(argc, argv[i+1]);
+        }
+    }
+    else{
+        perror("fork failed");
+    }
+}
+
+void run_pipe(int i, char **argv){
+    char buf[1024];
+    int p[2];
+    int pid;
+
+    /* open pipe */
+    if (pipe(p) == -1) {
+        perror ("pipe call failed");
+        exit(1);
+    }
+
+    pid = fork();
+
+    if (pid == 0) { /* child process */
+        close(p[0]);
+        if (dup2(p[1], STDOUT_FILENO) == -1) {
+            perror("dup2"); /* errno에 대응하는 메시지 출력됨 */
+            exit(1);
+        }
+        close(p[1]);
+        selectCmd(i, argv);
+        exit(0);
+    }
+    else if (pid > 0) {
+        wait(pid);
+        char *arg[1024];
+        close(p[1]);
+        sprintf(buf, "%d", p[0]);
+        arg[0] = argv[i + 2];
+        arg[1] = buf;
+        selectCmd(0, arg);
+    }
+    else
+        perror ("fork failed");
+}
+
+void selectCmd(int i, char **argv){
+    //argv 판별해서 맞는 명령 실행
+    if(!strcmp(argv[i], "cat")){
+        if(argv[i+1] == NULL){
+            fprintf(stderr, "A few argument..!\n");
+        }
+        ew_cat(i+1, argv[i+1]);
+    }
+    else if(!strcmp(argv[i], "pwd"))
+        ew_pwd();
+    else if(!strcmp(argv[i], "mkdir"))
+        ew_mkdir(i, argv[i+1]);
+    else if(!strcmp(argv[i], "rmdir"))
+        ew_rmdir(i, argv[i+1]);
+    else if(!strcmp(argv[i], "ln")){
+        if(argv[i+1] == NULL || argv[i+2] == NULL){
+            fprintf(stderr, "A few argument..!\n");
+        }
+        else{
+            ew_ln(argv[i+1], argv[i+2]);
+        }
+    }
+    else if(!strcmp(argv[i], "cp")){
+        if(argv[i+1] == NULL || argv[i+2] == NULL){
+            fprintf(stderr, "A few argument..!\n");
+        }
+        else{
+            my_cp(argv[i+1], argv[i+2]);
+        }
+    }
+    else if(!strcmp(argv[i], "rm")){
+        if(argv[i+1] == NULL){
+            fprintf(stderr, "A few argument..!\n");
+        }
+        else{
+            ew_rm(i, argv[i+1]);
+        }
+    }
+    else if(!strcmp(argv[i], "mv")){
+        if(argv[i+1] == NULL || argv[i+2] == NULL){
+            fprintf(stderr, "A few argument..!\n");
+        }
+        else{
+            my_mv(argv[i+1], argv[i+2]);
+        }
+    }
+    else{}
+}
+
+void ew_pwd(){
+    char buf[1024];
+    getcwd(buf, 1024);
+    printf("%s\n", buf);
+}
+
+void pwd_print(){
+    char buf[1024];
+    getcwd(buf, 1024);
+    printf("%s > ", buf);
+}
+
+void ew_ln(char *src, char *target){
+    if (link(src, target) <0){
+        perror("link");
+    }
+}
+
+void my_cp(char *src, char *target){
+    int src_fd; /* source file descriptor */
+    int dst_fd; /* destination file descriptor */
+    char buf[10];
+    ssize_t rcnt; /* read count */
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; /* == 0644 */
+    if ((src_fd = open(src, O_RDONLY)) == -1 ){
+        perror("src open"); /* errno에 대응하는 메시지 출력됨 */
+        exit(1);
+    }
+    if ( (dst_fd = creat(target, mode)) == -1 ){
+        perror("dst open"); /* errno에 대응하는 메시지 출력됨 */
+        exit(1);
+    }
+    while ( (rcnt = read(src_fd, buf, 10)) > 0){
+        write(dst_fd, buf, rcnt);
+    }
+    if (rcnt < 0) {
+        perror("read");
+        exit(1);
+    }
+    close(src_fd);
+    close(dst_fd);
+}
+
+void my_rm(char *target){
+    remove(target);
+}
+
+void my_mv(char *file, char *path){
+    my_cp(file, path);
+    my_rm(file);
 }
